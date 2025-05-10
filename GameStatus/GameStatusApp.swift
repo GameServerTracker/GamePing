d97d9ca8bf878287ac4fe5724ab665cfaa685570
Tom Rives <blitz@blitzlab.ninja>
2025-05-10 22:39:30 +0200

    Initial Commit

---
+++ GameStatus/GameStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStatusApp.swift
+//  GameStatus
+//
+//  Created by Tom on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct GameStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
